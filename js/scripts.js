0b9fc119e18c827a4b42306a23076fc55db07ced
JBorchers <79878454+JBorchers@users.noreply.github.com>
2021-03-19 15:46:34 -0600

    Cleared content to start new project

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,1 @@
-alert ("Hello world");
-
-let favoriteFood = 'Potatoes';
-document.write(favoriteFood);
-
 let pokemonList = [{name: "Charmander", height: .6, type: "Fire"},{name: "Squirtle",  height: .5, type: "Water"},{name: "Bulbasaur",  height: .7, type: "Grass"}];
