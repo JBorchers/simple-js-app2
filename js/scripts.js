e75a4af3e445cb44b3c91d065d7cd41a4dfb4dbb
JBorchers <79878454+JBorchers@users.noreply.github.com>
2021-03-22 15:08:40 -0600

    Created "for" loop for Pokemon list
    Displays list of pokemon names with their heights and a comment about height

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
-let pokemonList = [{name: "Charmander", height: .6, type: "Fire"},{name: "Squirtle",  height: .5, type: "Water"},{name: "Bulbasaur",  height: .7, type: "Grass"}];
+let pokemon = [
+    {name: "Charmander", height: 2, type: "fire"},
+    {name: "Oddish",  height: 1.6, type: "grass"},
+    {name: "Snorlax",  height: 6.9, type: "normal"}
+  ];
+
+for (let i=0; i < pokemon.length; i++){
+  if (pokemon[i].height <5){
+    document.write(pokemon[i].name + " (" + pokemon[i].height + ")" + " - this is a small pokemon");
+  }else {
+    document.write(pokemon[i].name + " (" + pokemon[i].height + ")" + " - wow that is a big pokemon!");
+  }
+}
