91658ed64767489b5f7ffbac411768f993600973
JBorchers <79878454+JBorchers@users.noreply.github.com>
2021-03-30 21:32:20 -0600

    Changed original "for" loop to a "forEach" function

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,28 @@
-let pokemon = [
+// let pokemon = [
+//     {name: "Charmander", height: 2, type: "fire"},
+//     {name: "Oddish",  height: 1.6, type: "grass"},
+//     {name: "Snorlax",  height: 6.9, type: "normal"}
+//   ];
+
+// for (let i=0; i < pokemon.length; i++){
+//   if (pokemon[i].height <5){
+//     document.write(pokemon[i].name + " (" + pokemon[i].height + " feet)" + " - this is a small pokemon<br>"
+//     );
+//   }else {
+//     document.write(pokemon[i].name + " (" + pokemon[i].height + " feet)" + " - wow that is a big pokemon!<br>");
+//   }
+// }
+
+let pokemonList = [
     {name: "Charmander", height: 2, type: "fire"},
     {name: "Oddish",  height: 1.6, type: "grass"},
     {name: "Snorlax",  height: 6.9, type: "normal"}
   ];
 
-for (let i=0; i < pokemon.length; i++){
-  if (pokemon[i].height <5){
-    document.write(pokemon[i].name + " (" + pokemon[i].height + " feet)" + " - this is a small pokemon<br>"
-    );
-  }else {
-    document.write(pokemon[i].name + " (" + pokemon[i].height + " feet)" + " - wow that is a big pokemon!<br>");
-  }
-}
+  pokemonList.forEach(function(pokemon) {
+    if (pokemon.height <5){
+      document.write(pokemon.name + " (" + pokemon.height + " feet)" + " - this is a small pokemon<br>"
+      );
+    }else {
+      document.write(pokemon.name + " (" + pokemon.height + " feet)" + " - wow that is a big pokemon!<br>"
+    })
