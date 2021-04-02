bc82b35c171b540885ce2dff93978327c6c548b0
JBorchers <79878454+JBorchers@users.noreply.github.com>
2021-04-01 21:11:17 -0600

    added event listeners to each button

--- js/scripts.js
+++ js/scripts.js
@@ -13,13 +13,32 @@ let pokemonRepository = (function () {
       pokemonList.push(item);
     }
 
+    function addListItem(pokemon) {
+      let pokemonList = document.querySelector ('.pokemon-list');
+      let listItem = document.createElement('li');
+      let button = document.createElement('button');
+      // use parameter in forEach to get pokemon.name
+      button.innerText = pokemon.name;
+      button.classList.add('button-class');
+      listItem.appendChild(button);
+      pokemonList.appendChild(listItem);
+      button.addEventListener('click', function (event) {
+        showDetails(pokemon);
+      });
+    }
+
+    function showDetails(pokemon) {
+      console.log(pokemon);
+    }
+
     return {
       getAll: getAll,
-      add: add
+      add: add,
+      addListItem: addListItem
     }
 
     })();
 
     pokemonRepository.getAll().forEach(function(pokemon) {
-      document.write(pokemon.name + ' is ' + pokemon.height + ' feet tall.<br>')
-    })
+      pokemonRepository.addListItem(pokemon);
+    });
